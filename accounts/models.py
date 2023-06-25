72909557e2b8d367a17cb97c5e9dc8b0b50e15aa
Senku01 <vigneshprakash2003@gmail.com>
2023-06-25 22:00:29 +0530

    Added Comments for better understanding

--- accounts/models.py
+++ accounts/models.py
@@ -4,11 +4,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Profile(models.Model):
+    # One-to-One relationship with the User model
     user = models.OneToOneField(User, on_delete = models.CASCADE)
+    # Authentication token for account verification
     auth_token = models.CharField(max_length=100)
-    is_verified = models.BooleanField(default=False)  
+    # Verification status of the user's account
+    is_verified = models.BooleanField(default=False) 
+    # Date and time when the profile is created
     created_at = models.DateTimeField(auto_now_add=True)
 
 
     def __str__(self):
+    # Return the username as the string representation
         return self.user.username
--- accounts/views.py
+++ accounts/views.py
@@ -10,34 +10,48 @@ from django.core.mail import send_mail
 
 # Create your views here.
 
-
+# View for the home page
 def home(request):
     return render(request , 'home.html')
 
 
+# View for the login attempt page
 def login_attempt(request):
     return render(request , 'login.html')
 
+
+# View for the registration attempt page
 def register_attempt(request):
 
     if request.method == 'POST':
         username = request.POST.get('username')
         email = request.POST.get('email')
         password = request.POST.get('password')
+        
+        # Using Try catch block for best practices
         try:
+            # Check if the username is already taken
             if User.objects.filter(username = username).first():
                 messages.success(request , 'Username is taken')
                 return redirect('/register')
+            
+            # Check if the email is already taken
             if User.objects.filter(email = email).first():
                 messages.success(request,'Email is Taken ')
                 return redirect('/register')
             
+
+            # Create a new User object and save it
             user_obj = User(username = username ,email = email)
             user_obj.set_password(password)
             user_obj.save()
+
+            # Generate an authentication token and create a new Profile object
             auth_token = str(uuid.uuid4())
             profile_obj = Profile.objects.create(user = user_obj , auth_token = auth_token)
             profile_obj.save()
+
+            # Send a verification email to the user
             send_mail_register(email,auth_token)
             
             return redirect('/token')
@@ -49,17 +63,25 @@ def register_attempt(request):
 
     return render(request , 'register.html')
 
+# View for the success page after successful registration
 
 def success(request):
     return render(request , 'success.html')
 
+# View for the token authentication page
+
 def token_auth(request):
     return render(request , 'token_auth.html')
+
+
+
+# View for verifying the user's account using the authentication token
 
 def verify(request , auth_token):
     try:
         profile_obj =  Profile.objects.filter(auth_token).first()
         if profile_obj:
+            # Set the account as verified
             profile_obj.is_verified = True
             profile_obj.save()
             messages.success(request , 'Your account has been verified')
@@ -69,10 +91,12 @@ def verify(request , auth_token):
     except Exception as e:
         print(e)
 
-
+# View for the error page
 def error_page(request):
     return render(request,'error.html')
 
+
+# Helper function to send a registration verification email
 def send_mail_register(email, token):
     subject = 'Your account need to be verified'
     message = f'Hi To verify your account http://127.0.0.1:8000/verify/{token}'
